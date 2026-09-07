b82e23fa800214934d633662d36a2e81f73340e7
torresalejandroaltamirano <torresalejandroaltamirano@gmail.com>
2026-09-07 20:22:45 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "cl.mascotadatos.app"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
